e0ca736e6d380758b0ccb78733055aafd4f9adf4
youessaitch <ayushkumar0077680@gmail.com>
2024-06-20 15:14:50 +0530

    first commit

---
+++ src/forms/user-profile-form/UserProfileForm.tsx
@@ -0,0 +1,141 @@
+import LoadingButton from "@/components/LoadingButton";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { User } from "@/types";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = z.object({
+  email: z.string().optional(), //optional means that the field is not required since email is already there while signup
+  name: z.string().min(1, "Name is required"),
+  addressLine1: z.string().min(1, "Address Line 1 is required"),
+  city: z.string().min(1, "City is required"),
+  country: z.string().min(1, "Country is required"),
+});
+
+type userFormData = z.infer<typeof formSchema>; //infer the type of the schema automatically using z.infer
+
+type Props = {
+  currentUser: User;
+  onSave: (userProfileData: userFormData) => void;
+  isLoading: boolean;
+};
+
+const userProfileForm = ({ onSave, isLoading, currentUser }: Props) => {
+  const form = useForm<userFormData>({
+    resolver: zodResolver(formSchema),
+    defaultValues: currentUser,
+  });
+
+  useEffect(() => {
+    form.reset(currentUser);
+  }, [currentUser, form]);
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSave)}
+        className="space-y-4 bg-gray-50 rounded-lg md:p-10"
+      >
+        <div>
+          <h2 className="text-2xl font-bold">User Profile Form</h2>
+          <FormDescription>
+            View and change your profile information here
+          </FormDescription>
+        </div>
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input {...field} disabled className="bg-white" />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input {...field} className="bg-white" />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <div className="flex flex-col md:flex-row gap-4">
+          <FormField
+            control={form.control}
+            name="addressLine1"
+            render={({ field }) => (
+              <FormItem className="flex-1">
+                <FormLabel>Address Line 1</FormLabel>
+                <FormControl>
+                  <Input {...field} className="bg-white" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="city"
+            render={({ field }) => (
+              <FormItem className="flex-1">
+                <FormLabel>City</FormLabel>
+                <FormControl>
+                  <Input {...field} className="bg-white" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="country"
+            render={({ field }) => (
+              <FormItem className="flex-1">
+                <FormLabel>Country</FormLabel>
+                <FormControl>
+                  <Input {...field} className="bg-white" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        {isLoading ? (
+          <LoadingButton />
+        ) : (
+          <Button type="submit" className="bg-red">
+            Submit
+          </Button>
+        )}
+      </form>
+    </Form>
+  );
+};
+
+//md means for medium screens
+
+export default userProfileForm;
